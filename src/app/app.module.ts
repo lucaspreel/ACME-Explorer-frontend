84725b0dc80d62ab1542b01753c9228bb2c8baba
lucaspreel <preel.lucas@gmail.com>
2022-03-29 20:58:39 +0200

    add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth'
 import { HeaderComponent } from './components/master/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDfw_5qjfLJgdEsjz-AB2-NrZ5UhXz7BQ",
+  authDomain: "acme-explorer-frontend-71af9.firebaseapp.com",
+  projectId: "acme-explorer-frontend-71af9",
+  storageBucket: "acme-explorer-frontend-71af9.appspot.com",
+  messagingSenderId: "612655358968"
+};
+
 
 @NgModule({
   declarations: [
@@ -15,9 +25,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
