d1c1fadaee0066ef1c68704a8b86400e7bf3ef9c
lucaspreel <preel.lucas@gmail.com>
2022-04-03 17:25:10 +0200

    add internationalization with i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth'
 import { HeaderComponent } from './components/master/header/header.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/security/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslatableComponent } from './components/shared/translatable/translatable.component';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCDfw_5qjfLJgdEsjz-AB2-NrZ5UhXz7BQ",
@@ -17,12 +20,17 @@ export const firebaseConfig = {
   messagingSenderId: "612655358968"
 };
 
+export function HttpLoaderFactory (http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    TranslatableComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +38,14 @@ export const firebaseConfig = {
     ReactiveFormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps : [HttpClient]
+      }
+    })
   ],
   providers: [AngularFireAuth],
   bootstrap: [AppComponent]
--- src/app/components/master/header/header.component.ts
+++ src/app/components/master/header/header.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "src/app/services/auth.service";
+import { TranslatableComponent } from '../../shared/translatable/translatable.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent extends TranslatableComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private translateService: TranslateService) {
+    super(translateService);
+  }
+
+  changeLanguage(language: string) {
+    super.changeLanguage(language);
+  }
 
   ngOnInit() {
   }
