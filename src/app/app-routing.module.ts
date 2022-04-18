20af2d458dfec95947d6183b38219fb59a2b6ee6
jesromper <95685631+jesromper@users.noreply.github.com>
2022-04-18 12:50:04 +0200

    Add: Modulo Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from "@angular/router";
-import { LoginComponent } from "./components/security/login/login.component";
-import { RegisterComponent } from "./components/security/register/register.component";
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/security/login/login.component';
+import { RegisterComponent } from './components/security/register/register.component';
 import { TripListComponent } from './components/trip/trip-list/trip-list.component';
 import { NotFoundPageComponent } from './components/shared/not-found-page/not-found-page.component';
 import { ActorRoleGuard } from './guards/actor-role.guard';
 import { DeniedAccesPageComponent } from './components/shared/denied-acces-page/denied-acces-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/trips', pathMatch: 'full'},
   {path: 'login', component: LoginComponent, canActivate: [ActorRoleGuard], data: {expectedRole: 'anonymous'}},
   {path: 'register', component: RegisterComponent, canActivate: [ActorRoleGuard], data: {expectedRole: 'anonymous'}},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [ActorRoleGuard], data: {expectedRole: 'anonymous'}},
   {path: 'trips', children: [
     {path: '', component: TripListComponent}
   ]},
