5eb1bd380fd6bdfb4a6e98124f9095013d3196ac
jesromper <95685631+jesromper@users.noreply.github.com>
2022-04-19 18:21:53 +0200

    Add:class estadistica

---
+++ src/app/components/dashboard/entities/estadistica.ts
@@ -0,0 +1,14 @@
+
+export class Estadistica {
+  Promedio: Number;
+  Máximo: Number;
+  Minimo: Number;
+  Desviación: Number;
+
+  constructor(Promedio: Number, Máximo: Number, Minimo: Number, Desviación: Number) {
+      this.Máximo = Máximo;
+      this.Minimo = Minimo;
+      this.Promedio = Promedio;
+      this.Desviación = Desviación;
+  }
+}
